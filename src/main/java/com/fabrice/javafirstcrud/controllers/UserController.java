b09b25d3db229ec00ad744791f683793c34b212d
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-08-15 19:57:08 +0200

    getting all users

--- src/main/java/com/fabrice/javafirstcrud/controllers/UserController.java
+++ src/main/java/com/fabrice/javafirstcrud/controllers/UserController.java
@@ -3,9 +3,12 @@ package com.fabrice.javafirstcrud.controllers;
 import com.fabrice.javafirstcrud.models.User;
 import com.fabrice.javafirstcrud.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController //anotation which adds rest api methods to this controller
 public class UserController {
@@ -16,7 +19,13 @@ public class UserController {
 
     //API to create new user
     @PostMapping("/user")
-    User saveNewUser(@RequestBody User newUser) {
+    User saveNewUser(@RequestBody User newUser) { //return type is User obj for this method
         return userRepository.save(newUser);
+    }
+
+    //API to get all users
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
