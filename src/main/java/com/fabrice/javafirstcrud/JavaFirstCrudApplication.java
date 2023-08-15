c04e40d3bcee426b0ec33ef6b2ad8222a31b2987
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-08-15 17:21:23 +0200

    initial commit

---
+++ src/main/java/com/fabrice/javafirstcrud/JavaFirstCrudApplication.java
@@ -0,0 +1,13 @@
+package com.fabrice.javafirstcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFirstCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFirstCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrice/javafirstcrud/JavaFirstCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrice.javafirstcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFirstCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
